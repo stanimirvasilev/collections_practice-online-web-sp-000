650ca10cabfd67bb2565227454b26fd77f2f37cf
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-02-20 17:13:16 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,49 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse 
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+  a.length <=> b.length
+end
+end
+
+def swap_elements(array)
+  array[0], array[1], array[2] = array[0], array[2], array[1]
+  
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |string| string[2] = "$"
+end
+array
+end
+
+
+def find_a(array)
+    array.select do |word|
+    word[0] == "a"
+end
+end
+
+def sum_array(array)
+array.inject(0, :+)
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element,index|
+    if element == "feet"
+      element
+    else element + "s"
+  end
+end
+
+end
