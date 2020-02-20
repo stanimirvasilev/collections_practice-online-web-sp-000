7871aef326e28955c84606164088e780658fed8b
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-02-20 14:28:22 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,32 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse 
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+  a.length <=> b.length
+end
+end
+
+def swap_elements(array)
+  array[0], array[1], array[2] = array[0], array[2], array[1]
+  
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |string| string[2] = "$"
+end
+array
+end
+
+def find_a(array)
+  array.each { |string| string.select ("a")
+end
